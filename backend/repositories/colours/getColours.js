3b7bbe30ed4ecf82bc9c3bb317f523d7a4a4698c
Hugo Ashjaei <hugo.ashjaei@pinmeto.com>
2025-02-17 17:53:23 +0100

    Sort colours and product types by name in retrieval functions

--- backend/repositories/colours/getColours.js
+++ backend/repositories/colours/getColours.js
@@ -8,7 +8,12 @@ const Colour = require("../../models/Colour.js");
  * @returns {Promise<Array<{ _id: string, name: string, hex: string }>>} An array of colour objects.
  */
 const getColours = async () => {
-  return Colour.find().select({ _id: 1, name: 1, hex: 1 }).lean();
+  return Colour.find()
+    .select({ _id: 1, name: 1, hex: 1 })
+    .sort({
+      name: 1,
+    })
+    .lean();
 };
 
 module.exports = {
--- backend/repositories/productTypes/getProductTypes.js
+++ backend/repositories/productTypes/getProductTypes.js
@@ -8,7 +8,12 @@ const ProductType = require("../../models/ProductType.js");
  * @returns {Promise<Array<{ _id: string, name: string }>>} An array of product type objects.
  */
 const getProductTypes = async () => {
-  return ProductType.find().select({ _id: 1, name: 1 }).lean();
+  return ProductType.find()
+    .select({ _id: 1, name: 1 })
+    .sort({
+      name: 1,
+    })
+    .lean();
 };
 
 module.exports = {
